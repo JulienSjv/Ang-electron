1e0551198ae0f6a7762299fa5e3a267d2f83103c
julienSjv <julien.stjeanvidal@gmail.com>
2018-04-17 17:32:44 +0200

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private _electronService: ElectronService) {}   // DI
+  
+  launchWindow() {
+    this._electronService.shell.openExternal('https://coursetro.com');
+  }
+
+  launchNotif() {
+    let myNotification = new Notification('Title', {
+      body: 'Test de notification',
+      icon: 'assets/favicon.ico'
+    })
+    myNotification.onclick = () => {
+      console.log('Notification clicked')
+    }
+    
+  }
+
+ 
+
 }
